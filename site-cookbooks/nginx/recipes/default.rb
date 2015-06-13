121ae4c63bd735c6b36c68d632be7c05310dad62
yuuho <itoyuho@gmail.com>
2015-06-13 21:20:45 +0900

    nginx更新

--- site-cookbooks/nginx/recipes/default.rb
+++ site-cookbooks/nginx/recipes/default.rb
@@ -15,3 +15,13 @@ service 'nginx' do
   supports :status=>true, :restart => true, :reload => true
   action [:enable, :start]
 end
+
+template "nginx.conf" do
+  path "/etc/nginx/nginx.conf"
+  source "nginx.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :reload,
+    'service[nginx]'
+end
